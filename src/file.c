c8841b0af3308027770439bf0b2fc99872b25cc0
DVM_khpte <147807946+DVMkhpte@users.noreply.github.com>
2024-09-30 20:48:43 +0200

    Update file.c --> createDatabase
    Ajout du USE apres la creation d'un fichier texte

--- src/file.c
+++ src/file.c
@@ -70,6 +70,26 @@ int createDatabase(char *filename) {
 
     fclose(file);
 
+    char res[4];
+    int8_t x = 1;
+    
+    while (x == 1) {
+        printf("Voulez-vous USE la base de données %s ? (Oui/Non) : ", filename);
+        scanf("%3s", res); 
 
+        if (strcmp(res, "O") == 0 || strcmp(res, "o") == 0 || strcmp(res, "oui") == 0 || strcmp(res, "Oui") == 0 || strcmp(res, "OUI") == 0) {
+            file = fopen(filepath, "r+"); 
+            if (!file) {
+                printf("Erreur lors de l'ouverture de la base de données\n");
+                exit(EXIT_FAILURE);
+            }
+            printf("Vous utilisez maintenant la base de données %s.\n", filename);
+            x = 0; 
+        } else if (strcmp(res, "N") == 0 || strcmp(res, "n") == 0 || strcmp(res, "non") == 0 || strcmp(res, "Non") == 0 || strcmp(res, "NON") == 0) {
+            return EXIT_SUCCESS; 
+        } else {
+            printf("Réponse invalide.\n");
+        }
+    }
 
 }
